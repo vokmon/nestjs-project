34ff21efcfc1b5775351f8150b56b988dad72763
Arnon Ruangthanawes <aruangthanawes@go7.io>
2024-10-04 07:51:34 +0700

    Add unit test for action guards

--- test/unit-test/decorator/action-guard.decorator.spec.ts
+++ test/unit-test/decorator/action-guard.decorator.spec.ts
@@ -1,6 +1,11 @@
+import { ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Actions } from '@src/decorators/actions.decorator';
+import { ActionsGuard } from '@src/guards/actions.guard';
+import { ACTION_ALL } from '@src/permissions/actions';
+import { vi } from 'vitest';
 
-describe('Action Guard Decorator', () => {
+describe('Action Decorator', () => {
   it('should set metadata on a class', () => {
     @Actions(['read', 'write'])
     class TestClass {}
@@ -49,5 +54,65 @@ describe('Action Guard Decorator', () => {
 
     const actions = Reflect.getMetadata(Actions.KEY, TestClass);
     expect(actions).toEqual(['read']);
+  });
+});
+
+describe('Action Guard', () => {
+  let guard: ActionsGuard;
+  let reflector: Reflector;
+  let mockExecutionContext: ExecutionContext;
+
+  beforeEach(() => {
+    reflector = {
+      get: vi.fn(),
+    } as any;
+
+    guard = new ActionsGuard(reflector);
+
+    mockExecutionContext = {
+      getHandler: vi.fn(),
+      switchToHttp: vi.fn().mockReturnValue({
+        getRequest: vi.fn().mockReturnValue({
+          user: {
+            actions: [],
+          },
+        }),
+      }),
+    } as any;
+  });
+
+  it('should be defined', () => {
+    expect(guard).toBeDefined();
+  });
+
+  it('should return false if no actions are defined', () => {
+    vi.spyOn(reflector, 'get').mockReturnValue(undefined);
+    expect(guard.canActivate(mockExecutionContext)).toBe(false);
+  });
+
+  it('should return true if user has ACTION_ALL', () => {
+    vi.spyOn(reflector, 'get').mockReturnValue(['read']);
+    mockExecutionContext.switchToHttp().getRequest().user.actions = [
+      ACTION_ALL.name,
+    ];
+    expect(guard.canActivate(mockExecutionContext)).toBe(true);
+  });
+
+  it('should return true if user has required action', () => {
+    vi.spyOn(reflector, 'get').mockReturnValue(['read']);
+    mockExecutionContext.switchToHttp().getRequest().user.actions = ['read'];
+    expect(guard.canActivate(mockExecutionContext)).toBe(true);
+  });
+
+  it('should return false if user does not have required action', () => {
+    vi.spyOn(reflector, 'get').mockReturnValue(['write']);
+    mockExecutionContext.switchToHttp().getRequest().user.actions = ['read'];
+    expect(guard.canActivate(mockExecutionContext)).toBe(false);
+  });
+
+  it('should return true if user has at least one of the required actions', () => {
+    vi.spyOn(reflector, 'get').mockReturnValue(['read', 'write']);
+    mockExecutionContext.switchToHttp().getRequest().user.actions = ['write'];
+    expect(guard.canActivate(mockExecutionContext)).toBe(true);
   });
 });
